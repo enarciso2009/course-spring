11d5cdacaf0bc6379ff3c3c7115151a545610488
enarciso2009 <enarciso2009@gmail.com>
2019-09-21 04:08:22 -0300

    Category

---
+++ src/main/java/com/narcsoft/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.narcsoft.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.narcsoft.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
